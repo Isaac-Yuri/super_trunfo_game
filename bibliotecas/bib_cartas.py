2bfd6845ef0505fee75a989881a23f0236247bc0
Isaac Yuri <isaacyuri22@gmail.com>
2022-12-11 08:21:24 -0300

    função calcula_pontos_jogador() transferida para outro arquivo

--- bibliotecas/bib_cartas.py
+++ bibliotecas/bib_cartas.py
@@ -25,29 +25,3 @@ def gera_baralho(personagens):
         baralho.append(carta) #Na lista do baralho é adicionado a carta que é uma sub-lista
     return baralho
 
-
-def calcula_pontos_jogador(pontos_jogador, habilidade_vencedora, habilidade_perdedora, resultado=True): 
-    """
-    Calcula os pontos do jogador:
-        pontos_jogador:
-            Os pontos atual do jogador que se quer calcular
-        habilidade_vencedora:
-            Pontos da característica vencedora da rodada
-        habilidade_perdedora:
-            Pontos da característica perdedora da rodada
-        resultado:
-            Se o resultado for True:
-                Calcula os pontos do vencedor:
-                    Soma-se a diferença aos pontos do vencedor
-            Se o resultador não for True:
-                Calcula os pontos do perdedor:
-                    Subtrai-se a diferença aos pontos do perdedor
-    """
-    diferenca_habilidade = habilidade_vencedora - habilidade_perdedora #Calcula a diferença entre as habilidades
-    if (resultado):
-        pontos_vencedor = pontos_jogador + diferenca_habilidade
-        return pontos_vencedor
-    else:
-        pontos_perdedor = pontos_jogador - diferenca_habilidade
-        return pontos_perdedor
-    
