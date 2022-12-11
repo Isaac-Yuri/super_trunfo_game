1eccfc9775bf601e8fa149dd10b211bc084afc24
Isaac Yuri <isaacyuri22@gmail.com>
2022-12-11 07:27:09 -0300

    arquivo principal adicionado

---
+++ main.py
@@ -0,0 +1,6 @@
+from bibliotecas.bib_cartas import *
+
+personagens = ["Naruto", "Sakura", "Sasuke", "Itachi", "Madara", "Obito ", "Hidan ", "Sasori", "Pain  ", "Konan ", "Jiraya", "Yamato", "Gaara ", "Kiba  ", "Hinata", "Choji ", "Neji  ", "Temari", "Asuma ", "Kakuzu"]
+
+baralho = gera_baralho(personagens)
+
