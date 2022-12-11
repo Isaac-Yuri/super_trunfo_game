a1dd1bdb6f2f4a2fdcb44b9e7f0c9daab922a3b3
Isaac Yuri <isaacyuri22@gmail.com>
2022-12-11 08:20:27 -0300

    função cria_jogador() criada

--- bibliotecas/bib_funcionalidades.py
+++ bibliotecas/bib_funcionalidades.py
@@ -42,3 +42,41 @@ def carta_usada(baralho):
     #para colar a carta usada no fim do baralho
     baralho.append(baralho[0])#coloca na ultima posição
     del baralho[0]#apaga oque ficou na primeira
+
+
+def calcula_pontos_jogador(pontos_jogador, habilidade_vencedora, habilidade_perdedora, resultado=True): 
+    """
+    Calcula os pontos do jogador:
+        pontos_jogador:
+            Os pontos atual do jogador que se quer calcular
+        habilidade_vencedora:
+            Pontos da característica vencedora da rodada
+        habilidade_perdedora:
+            Pontos da característica perdedora da rodada
+        resultado:
+            Se o resultado for True:
+                Calcula os pontos do vencedor:
+                    Soma-se a diferença aos pontos do vencedor
+            Se o resultador não for True:
+                Calcula os pontos do perdedor:
+                    Subtrai-se a diferença aos pontos do perdedor
+    """
+    diferenca_habilidade = habilidade_vencedora - habilidade_perdedora #Calcula a diferença entre as habilidades
+    if (resultado):
+        pontos_vencedor = pontos_jogador + diferenca_habilidade
+        return pontos_vencedor
+    else:
+        pontos_perdedor = pontos_jogador - diferenca_habilidade
+        return pontos_perdedor
+
+
+def cria_jogador(baralho, quantidade_pontos=1000) -> [list(), int()]:
+    """
+    Parâmetros:
+        baralho: Pilha que se deseja retirar as cartas.
+        quantidade_pontos: Número de pontos que o jogador vai ter que por padrão é 1000.
+    Retorno:
+        Retorna uma lista com as cartas e com a quantidade de pontos do jogador 
+    """
+    cartas_do_jogador = distribui_cartas(baralho, 4)
+    return [cartas_do_jogador, quantidade_pontos]
